88d10cedcc608c63085548719e5fc0ebbd1a6b96
Kai Iyer <kaiiyer47@yahoo.in>
2019-01-20 01:03:31 +0530

    Add files via upload

---
+++ Active-Response/atd_subscriber.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import sys
+import time
+import json
+import threading
+import importlib
+
+from dxlclient.callbacks import EventCallback
+from dxlclient.client import DxlClient
+from dxlclient.client_config import DxlClientConfig
+from dxlclient.message import Event, Request
+from elasticsearch import Elasticsearch
+
+# Import common logging and configuration
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
+#from common import *
+
+# Configure local logger
+logging.getLogger().setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
+
+CONFIG_FILE = "/home/kai/Documents/Pen/OpenDXL-ATD-MAR-Elasticsearch/config/dxlclient1.config"
+config = DxlClientConfig.create_dxl_config_from_file(CONFIG_FILE)
+
+# Variable MISP python
+marc1search = importlib.import_module("marc1")
+marc2search = importlib.import_module("marc2")
+
+# Elasticsearch setup
+es = Elasticsearch(['http://http://localhost:9200'])
+
+# Create the client
+with DxlClient(config) as client:
+
+    # Connect to the fabric
+    client.connect()
+
+    # Create and add event listener
+    class MyEventCallback(EventCallback):
+        def on_event(self, event):
+            try:
+                query = event.payload.decode()
+                logger.info("Event received: " + query)
+
+                # The ATD json can sometimes have non-standard json characters
+                # at the end. Let's clean it up.
+                # End the json at the last curly brace: }
+                query = query[:query.rfind('}')+1]
+                query = json.loads(query)
+                
+                # Push ATD analysis data into Elasticsearch / Kibana
+                res = es.index(index='atd', doc_type='report', body=query)
+                # Run Active Response query. Parsing out md5 hashes included in the DXL message
+                md5 = query['Summary']['Subject']['md5']
+                md5main = query['Summary']['Subject']['md5']
+                if not md5: 
+                   pass
+                else: 
+                   marc1search.action(md5, md5main)
+                   marc2search.action(md5, md5main)
+               
+                for hashes in query['Summary']['Files']:
+                    md5 = hashes['Md5']
+                    if not md5: 
+                       pass
+                    else: 
+                       marc1search.action(md5, md5main)
+                       marc2search.action(md5, md5main)
+                
+            except Exception as e:
+                print(e)
+
+        @staticmethod
+        def worker_thread(req):
+            client.sync_request(req)
+
+    # Register the callback with the client
+    client.add_event_callback('#', MyEventCallback(), subscribe_to_topic=False)
+    client.subscribe("/mcafee/event/atd/file/report")
+
+    # Wait forever
+    while True:
+        time.sleep(60)
+
